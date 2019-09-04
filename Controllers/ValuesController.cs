078ecbab66d8893a53d930069f337cb150fd123d
karabuluthakan <hakan.karabulut@ugurbilgi.net>
2019-09-04 17:59:38 +0300

    basic

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace elk.Controllers
 {
@@ -10,10 +11,18 @@ namespace elk.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            _logger.LogInformation("// GET api/values");
             return new string[] { "value1", "value2" };
         }
 
